a23866e389058cca1d71f665f3a56aed5e240b6b
kn-shimada <xianrendaotian012@gmail.com>
2021-02-26 13:09:50 +0900

    test

--- perfect.c
+++ perfect.c
@@ -2,12 +2,12 @@
 #include <math.h>
 #include <stdint.h>
 //メルセンヌ素数を利用、奇数の完全数は見つかってないので省く
-​
+
 int main(void)
 {
     uint16_t i, j, k, prime, perfect;
     i = 2;
-​
+
     while(i <= 13)
     {
         prime = (uint16_t)pow(2, i) - 1;
@@ -20,7 +20,7 @@ int main(void)
 				    break;
 			    }
 		    }
-​
+            
 		    if(k == 0)  //prime素数ならperfectを表示
             {
                 perfect = (uint16_t)pow(2, i - 1) * (pow(2, i) - 1); 
