e985b39b48cd75673c1fd10250841d9b6b5ae7f5
kn-shimada <xianrendaotian012@gmail.com>
2021-07-06 14:27:37 +0900

    update: prime_check

--- prime_check.c
+++ prime_check.c
@@ -1,25 +1,26 @@
 # include<stdio.h>
+# include <stdint.h>
 # include<math.h>
 
-int prime(int num);
+int prime(uint_fast16_t num);
 
 int main(void){
-    int num;
+    uint_fast16_t num;
 
     printf("number: ");
     scanf("%d", &num);
 
     if (prime(num) == 0) {
-        printf("%d is prime number", num);
+        printf("%d is prime number\n", num);
     } else {
-        printf("%d isn't prime number", num);
+        printf("%d isn't prime number\n", num);
     }
 
     return 0;
 }
 
-int prime(int num) {
-    int i;
+int prime(uint_fast16_t num) {
+    uint_fast16_t i;
 
     if (num == 2){
         return 0;
